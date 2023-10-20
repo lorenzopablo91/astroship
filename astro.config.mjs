b40de9b7ad8d1bb0b74c884429a2e2a3aff0178a
unknown <lorenzopablo91@gmail.com>
2023-10-20 15:23:04 -0300

    Upgrade to Astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
@@ -9,9 +8,6 @@ export default defineConfig({
   site: "https://legalesdw.com",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     mdx(),
     sitemap(),
   ],
