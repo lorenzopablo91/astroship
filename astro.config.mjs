11bb009da76017fab8e2577c6b20ecc30ff725f8
unknown <lorenzopablo91@gmail.com>
2023-10-21 02:37:22 -0300

    add vercel analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   site: "https://legalesdw.com",
   integrations: [
     tailwind(),
